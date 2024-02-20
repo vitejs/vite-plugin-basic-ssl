2266824312e929041dff6f37e3ed945aeb4e40e9
Wojciech Maj <kontakt@wojtekmaj.pl>
2024-02-20 15:00:33 +0100

    fix: update unbuild to 2.0.0, fix type declarations for ESM (#28)
    * fix: update unbuild to 2.0.0, fix type declarations for ESM
    
    Fixes #24
    
    * Make reserved names ES3 compliant

--- build.config.ts
+++ build.config.ts
@@ -4,9 +4,14 @@ export default defineBuildConfig({
   entries: ['src/index'],
   externals: ['vite'],
   clean: true,
-  declaration: true,
+  declaration: 'compatible',
   rollup: {
     emitCJS: true,
-    inlineDependencies: true
+    inlineDependencies: true,
+    output: {
+      generatedCode: {
+        reservedNamesAsProps: false
+      }
+    }
   }
 })
