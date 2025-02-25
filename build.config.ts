1a07baab7d8953424df97c4f7d4b761087a160eb
bluwy <bjornlu.dev@gmail.com>
2025-02-25 21:12:23 +0800

    feat!: only support vite 6+ and node 18+

--- build.config.ts
+++ build.config.ts
@@ -1,20 +1,40 @@
+import fs from 'node:fs/promises'
 import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
   entries: ['src/index'],
   externals: ['vite'],
   clean: true,
-  declaration: 'compatible',
+  declaration: true,
   rollup: {
     emitCJS: true,
     inlineDependencies: true,
     esbuild: {
-      target: 'node14.21.3',
+      target: 'node18',
     },
     output: {
       generatedCode: {
         reservedNamesAsProps: false,
       },
+    },
+  },
+  hooks: {
+    async 'build:done'(ctx) {
+      // Remove duplicated `dist/chunks/certificate.{cjs,mjs}` chunks
+      // as both are only dynamically imported by the entrypoints. The
+      // dynamic import can use the `.mjs` chunk only instead.
+      await fs.rm('dist/chunks/certificate.cjs')
+      const indexCjs = await fs.readFile('dist/index.cjs', 'utf8')
+      const editedIndexCjs = indexCjs.replace(
+        'chunks/certificate.cjs',
+        'chunks/certificate.mjs',
+      )
+      if (indexCjs === editedIndexCjs) {
+        throw new Error(
+          'Failed to find `dist/chunks/certificate.cjs` in `dist/index.cjs`',
+        )
+      }
+      await fs.writeFile('dist/index.cjs', editedIndexCjs)
     },
   },
 })
