23b91d223dda02076431056de9b1fa6687874f0a
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2026-01-08 11:33:03 +0900

    chore: fix

--- scripts/release.ts
+++ scripts/release.ts
@@ -5,11 +5,11 @@ import { logRecentCommits } from './releaseUtils'
 release({
   repo: 'vite-plugin-basic-ssl',
   packages: ['plugin-basic-ssl'],
-  getPkgDir: () => '',
+  getPkgDir: () => '.',
   toTag: (_pkg, version) => `v${version}`,
   logChangelog: (pkg) => logRecentCommits(pkg),
   generateChangelog: async (_pkgName) => {
     console.log(colors.cyan('\nGenerating changelog...'))
-    await generateChangelog({ getPkgDir: () => '' })
+    await generateChangelog({ getPkgDir: () => '.' })
   },
 })
